4c15ede6f65e8399f73d790a22d2aa79ea33b759
sameerrajputra <sameer.rajputra55@gmail.com>
2018-08-03 16:31:46 +0545

    Added testing for user login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -225,7 +225,7 @@ describe('POST /users', () => {
           expect(user.email).toBe(email);
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => done());
      });
   });
 
@@ -261,5 +261,52 @@ describe('POST /users', () => {
      .end(done);
 
 });
+
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+
+    request(app)
+     .post('/users/login')
+     .send({email: users[0].email, password: users[0].password})
+     .expect(200)
+     .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+     })
+     .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.header['x-auth']
+        });
+        done();
+      }).catch((e) => done());
+     });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+     .post('/users/login')
+     .send({email: users[0].email, password: 'metalheasdfasd'})
+     .expect(400)
+     .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+     })
+     .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done());
+     });
+  });
 
 });
